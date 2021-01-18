1437a439b437b180a1c66d913ff3db792877945f
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-18 17:29:43 +0200

    Q4 answer

---
+++ Q4.js
@@ -0,0 +1,24 @@
+let num1 = -5;
+let num2 = -2;
+let num3 = -6;
+let num4 = 0;
+let num5 = -1;
+if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+    alert (num1);
+}
+
+if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5) {
+    alert (num2);
+}
+
+if (num3 > num2 && num3 > num1 && num3 > num4 && num3 > num5) {
+    alert (num3);
+}
+
+if (num4 > num2 && num4 > num3 && num4 > num1 && num4 > num5) {
+    alert (num4);
+}
+
+if (num5 > num2 && num5 > num3 && num5 > num4 && num5 > num1) {
+    alert (num5);
+}
