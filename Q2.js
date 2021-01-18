e86eefdccd9093175954b53faa8e4ac44bef30df
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-18 16:57:15 +0200

    Q2 answer

---
+++ Q2.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = -2;
+let num3 = -3;
+
+if (num1 < 0 && num2 < 0 && num3 < 0) {
+    alert ('The sign is -');
+} else if (num1 < 0 && num2 > 0 && num3 > 0) {
+    alert ('The sign is -');
+    } else if (num1 > 0 && num2 < 0 && num3 > 0) {
+        alert ('The sign is -');
+        } else if (num1 > 0 && num2 > 0 && num3 < 0) {
+            alert ('The sign is -');
+            } else alert ('The sign is +');
+
