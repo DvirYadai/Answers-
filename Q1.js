3f3b2824de307b19632a61b5a047ca3f564a1968
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-18 16:16:26 +0200

    Q1 answer

---
+++ Q1.js
@@ -0,0 +1,7 @@
+let num1 = prompt ('Enter your first number');
+num1 = Number(num1);
+let num2 = prompt ('Enter your second number');
+num2 = Number(num2);
+if (num1 > num2) {
+    alert ('The larger number is: ' + num1);
+} else alert ('The larger number is: ' + num2);
