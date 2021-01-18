1569679465f6f4ef6e1026e6322c874f56f90c03
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 00:46:07 +0200

    Update Q8.js

--- Q8.js
+++ Q8.js
@@ -1,14 +1,13 @@
 function toDigit(num){
     let arr = [];
-    let i = 1;
     while(num > 0){
-        arr.unshift((num % 10) * i);
+        if(((num % 10)) !== 0){
+            arr.unshift((num % 10));
+        }
         num = Math.floor(num / 10);
-        i *= 10;
     }
     return arr;
 }
-
 
 function happyNumber(num){
     let arrNumbers = toDigit(num);
@@ -16,14 +15,28 @@ function happyNumber(num){
     for(let i = 0; i < arrNumbers.length; i++){
         tempSum += (arrNumbers[i]**2);
     }
-    while(tempSum != 1){
+    let arrChack = [];
+    while(tempSum !== 1){
         arrNumbers = toDigit(tempSum);
+        arrChack.push(tempSum);
         tempSum = 0;
         for(let i = 0; i < arrNumbers.length; i++){
             tempSum += (arrNumbers[i]**2);
         }
+        for(let j = 0; j < arrChack.length; j++){
+            if(tempSum === arrChack[j])
+                return false;
+        }
     }
     if(tempSum === 1){
-        return true;
-    } else return false;
+        return true; 
+}
+}
+
+let count = 0;
+for(let i = 1; count < 5; i++){
+    if (happyNumber(i)){
+        count++;
+        console.log(i);
+    }
 }
